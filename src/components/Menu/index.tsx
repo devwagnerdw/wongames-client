edeefb57408bfda3a896832fe3a05f89d9c576b2
vagnealves <vagnerdw1@gmail.com>
2023-11-02 21:42:44 -0300

    Add styles to MenuFull

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,12 +4,12 @@ import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outline
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
+import Button from 'components/Button'
 import Logo from 'components/Logo'
 import * as S from './styles'
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false)
-
   return (
     <S.Wrapper>
       <S.IconWrapper onClick={() => setIsOpen(true)}>
@@ -29,9 +29,22 @@ const Menu = () => {
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   )
 }
-
 export default Menu
