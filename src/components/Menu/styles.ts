c62f84aafe82fbd40f39b66f88516617fcbfdf0e
vagnealves <vagnerdw1@gmail.com>
2023-11-03 00:03:46 -0300

    Ajuste links menu mobile

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -33,7 +33,13 @@ export const MenuGroup = styled.div`
     }
   `}
 `
-export const MenuNav = styled.div``
+export const MenuNav = styled.div`
+  ${({ theme }) => css`
+    ${media.greaterThan('medium')`
+			margin-left: ${theme.spacings.small};
+		`}
+  `}
+`
 export const MenuLink = styled.a`
   ${({ theme }) => css`
     position: relative;
