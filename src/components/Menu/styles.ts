d8b69cc66c51fb5a464a5820568991fde77314a1
vagnealves <vagnerdw1@gmail.com>
2023-11-03 23:04:58 -0300

    Add Menu desktop changes

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -28,11 +28,13 @@ export const MenuGroup = styled.div`
     display: flex;
     flex-grow: 1;
     justify-content: flex-end;
+    align-items: center;
     > div {
       margin-left: ${theme.spacings.xsmall};
     }
   `}
 `
+
 export const MenuNav = styled.div`
   ${({ theme }) => css`
     ${media.greaterThan('medium')`
@@ -40,9 +42,11 @@ export const MenuNav = styled.div`
 		`}
   `}
 `
+
 export const MenuLink = styled.a`
   ${({ theme }) => css`
     position: relative;
+    color: ${theme.colors.white};
     font-size: ${theme.font.sizes.medium};
     margin: 0.3rem ${theme.spacings.small} 0;
     text-decoration: none;
