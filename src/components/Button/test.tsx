75ba49f3e8f61ce104225328649f71032090d434
vagnealves <vagnerdw1@gmail.com>
2023-11-05 22:51:45 -0300

    Add Button to support anchor/button types

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,22 +1,17 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
-
 import Button from '.'
-
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
     const { container } = renderWithTheme(<Button>Buy now</Button>)
-
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
-
     expect(container.firstChild).toMatchSnapshot()
   })
-
   it('should render the small size', () => {
     renderWithTheme(<Button size="small">Buy now</Button>)
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
@@ -38,13 +33,24 @@ describe('<Button />', () => {
       width: '100%'
     })
   })
-
   it('should render an icon version', () => {
     renderWithTheme(
       <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
     )
-
     expect(screen.getByText(/buy now/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render Button as a link', () => {
+    renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>
+    )
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
   })
 })
