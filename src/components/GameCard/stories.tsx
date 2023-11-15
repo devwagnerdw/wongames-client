befde9603475004f9e8253e72d4a6fde04f22cd4
vagnealves <vagnerdw1@gmail.com>
2023-11-14 21:12:54 -0300

    Add promotional price and tests

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,6 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import GameCard, { GameCardProps } from '.'
-
 export default {
   title: 'GameCard',
   component: GameCard,
@@ -8,7 +7,13 @@ export default {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 'R$ 235,00'
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
   }
 } as Meta
 
