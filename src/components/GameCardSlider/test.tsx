7f709445075073b16dc6629e9c779a8b6fc70734
vagnealves <vagnerdw1@gmail.com>
2023-11-16 19:09:54 -0300

    Add GameCardSlider styles

--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -1,8 +1,8 @@
 import 'match-media-mock'
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCardSlider from '.'
-
 const items = [
   {
     title: 'Population Zero',
@@ -40,10 +40,20 @@ const items = [
     promotionalPrice: 'R$ 215,00'
   }
 ]
-
 describe('<GameSlider />', () => {
   it('should render with 4 active items', () => {
     const { container } = renderWithTheme(<GameCardSlider items={items} />)
     expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should render white arrows if color passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
   })
 })
