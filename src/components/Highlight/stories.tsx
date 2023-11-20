2169377344c1c16926037f82e73572088160a330
vagnealves <vagnerdw1@gmail.com>
2023-11-20 14:55:08 -0300

    Add getServerSideProps method and create mocks

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,15 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
+import item from './mock'
+
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Read Dead is back!',
-    subtitle: 'Come see John’s new adventures',
-    backgroundImage: '/img/red-dead-img.jpg',
-    buttonLabel: 'Buy now',
-    buttonLink: '/games/rdr2'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => (
@@ -17,13 +13,11 @@ export const Default: Story<HighlightProps> = (args) => (
     <Highlight {...args} />
   </div>
 )
-
 export const WithFloatImage: Story<HighlightProps> = (args) => (
   <div style={{ maxWidth: '104rem' }}>
     <Highlight {...args} />
   </div>
 )
-
 WithFloatImage.args = {
   floatImage: '/img/red-dead-float.png'
 }
