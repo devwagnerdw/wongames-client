0bf2890f5389fea694072804b15b6520f99479bc
vagnealves <vagnerdw1@gmail.com>
2023-11-25 15:55:10 -0300

    Fixes Tab accessibility test for Checkbox

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -52,4 +52,14 @@ describe('<Checkbox />', () => {
     })
     expect(onCheck).toHaveBeenCalledWith(false)
   })
+
+  it('should be accessible with tab', async () => {
+    renderWithTheme(<Checkbox label="Checkbox" labelFor="Checkbox" />)
+
+    expect(document.body).toHaveFocus()
+
+    await userEvent.tab()
+
+    expect(screen.getByLabelText(/checkbox/i)).toHaveFocus()
+  })
 })
