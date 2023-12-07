5c7bd710dfc0c313be22063dd50bd301b130cc40
vagnealves <vagnerdw1@gmail.com>
2023-12-07 20:40:12 -0300

    Refactor TextField

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,41 +1,35 @@
 import { useState, InputHTMLAttributes } from 'react'
-
 import * as S from './styles'
-
 export type TextFieldProps = {
   onInput?: (value: string) => void
   label?: string
+  initialValue?: string
   icon?: React.ReactNode
   iconPosition?: 'left' | 'right'
   disabled?: boolean
-  labelFor?: string
-  initialValue?: string
   error?: string
 } & InputHTMLAttributes<HTMLInputElement>
-
 const TextField = ({
-  label,
   icon,
-  labelFor = '',
-  disabled = false,
   iconPosition = 'left',
+  label,
+  name,
   initialValue = '',
   error,
+  disabled = false,
   onInput,
   ...props
 }: TextFieldProps) => {
   const [value, setValue] = useState(initialValue)
-
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.currentTarget.value
     setValue(newValue)
-
     !!onInput && onInput(newValue)
   }
 
   return (
     <S.Wrapper disabled={disabled} error={!!error}>
-      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      {!!label && <S.Label htmlFor={name}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
         <S.Input
@@ -44,6 +38,8 @@ const TextField = ({
           value={value}
           iconPosition={iconPosition}
           disabled={disabled}
+          name={name}
+          {...(label ? { id: name } : {})}
           {...props}
         />
       </S.InputWrapper>
@@ -51,5 +47,4 @@ const TextField = ({
     </S.Wrapper>
   )
 }
-
 export default TextField
