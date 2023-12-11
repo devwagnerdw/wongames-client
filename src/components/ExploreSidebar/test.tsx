b01864ecc968fc4220805c85adda75ac530e7df9
vagnealves <vagnerdw1@gmail.com>
2023-12-10 21:09:41 -0300

    Add Explore/Games page

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -1,10 +1,8 @@
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
-import { css } from 'styled-components'
 
 import ExploreSidebar from '.'
-import { Overlay } from './styles'
 
 import items from './mock'
 
@@ -62,65 +60,6 @@ describe('<ExploreSidebar />', () => {
         onFilter={onFilter}
       />
     )
-
     userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
-    expect(onFilter).toBeCalledWith({ windows: true, sort_by: 'low-to-high' })
-  })
-
-  it('should filter with checked values', () => {
-    const onFilter = jest.fn()
-
-    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />)
-
-    userEvent.click(screen.getByLabelText(/windows/i))
-    userEvent.click(screen.getByLabelText(/linux/i))
-    userEvent.click(screen.getByLabelText(/low to high/i))
-
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
-    expect(onFilter).toBeCalledWith({
-      windows: true,
-      linux: true,
-      sort_by: 'low-to-high'
-    })
-  })
-
-  it('should altern between radio options', () => {
-    const onFilter = jest.fn()
-
-    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />)
-
-    userEvent.click(screen.getByLabelText(/low to high/i))
-    userEvent.click(screen.getByLabelText(/high to low/i))
-
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
-    expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' })
-  })
-
-  it('should open/close sidebar when filtering on mobile ', () => {
-    const { container } = renderWithTheme(
-      <ExploreSidebar items={items} onFilter={jest.fn} />
-    )
-
-    const variant = {
-      media: '(max-width:768px)',
-      modifier: String(css`
-        ${Overlay}
-      `)
-    }
-
-    const Element = container.firstChild
-
-    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
-
-    userEvent.click(screen.getByLabelText(/open filters/))
-
-    expect(Element).toHaveStyleRule('opacity', '1', variant)
-
-    userEvent.click(screen.getByLabelText(/close filters/))
-
-    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
   })
 })
