0285f3185b853c2f96e4e6fbd26c9e48fbd07bf9
vagnealves <vagnerdw1@gmail.com>
2023-12-12 20:33:40 -0300

    Add UserDropdown tests

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,13 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import UserDropdown from '.'
 
 describe('<UserDropdown />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<UserDropdown />)
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
+    expect(screen.getByText(/willian/i)).toBeInTheDocument()
+  })
 
-    expect(screen.getByRole('heading', { name: /UserDropdown/i })).toBeInTheDocument()
+  it('should render the menu', async () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
 
-    expect(container.firstChild).toMatchSnapshot()
+    // Open menu
+    userEvent.click(screen.getByText(/willian/i))
+
+    // Use 'findByRole' to wait for the menu items to appear
+    const profileLink = await screen.findByRole('link', { name: /my profile/i })
+    const wishlistLink = await screen.findByRole('link', { name: /wishlist/i })
+    const signOutLink = await screen.findByRole('link', { name: /sign out/i })
+
+    // Assert that the menu items are present
+    expect(profileLink).toBeInTheDocument()
+    expect(wishlistLink).toBeInTheDocument()
+    expect(signOutLink).toBeInTheDocument()
   })
 })
