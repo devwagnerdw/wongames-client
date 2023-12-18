6c53c8f645cb5f9f4f3235a5e5d8f44778dcf89f
vagnealves <vagnerdw1@gmail.com>
2023-12-18 17:18:36 -0300

    Add types to our QueryGames

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,25 +1,27 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import filterItemsMock from 'components/ExploreSidebar/mock'
+
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query({
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: { limit: 9 }
   })
-
   return {
     props: {
       revalidate: 60,
       games: data.games.map((game) => ({
         title: game.name,
         developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover.url}`,
+        img: `http://localhost:1337${game.cover!.url}`,
         price: new Intl.NumberFormat('en', {
           style: 'currency',
           currency: 'USD'
