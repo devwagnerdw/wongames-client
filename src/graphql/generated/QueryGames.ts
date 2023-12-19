25920a734fa4139f33c8c8204450082a154b6153
vagnealves <vagnerdw1@gmail.com>
2023-12-18 22:03:42 -0300

    Create dynamic Game page

--- src/graphql/generated/QueryGames.ts
+++ src/graphql/generated/QueryGames.ts
@@ -8,28 +8,28 @@
 // ====================================================
 
 export interface QueryGames_games_cover {
-    __typename: "UploadFile";
-    url: string;
-  }
-  
-  export interface QueryGames_games_developers {
-    __typename: "Developer";
-    name: string;
-  }
-  
-  export interface QueryGames_games {
-    __typename: "Game";
-    name: string;
-    slug: string;
-    cover: QueryGames_games_cover | null;
-    developers: QueryGames_games_developers[];
-    price: number;
-  }
-  
-  export interface QueryGames {
-    games: QueryGames_games[];
-  }
-  
-  export interface QueryGamesVariables {
-    limit: number;
-  }
+  __typename: "UploadFile";
+  url: string;
+}
+
+export interface QueryGames_games_developers {
+  __typename: "Developer";
+  name: string;
+}
+
+export interface QueryGames_games {
+  __typename: "Game";
+  name: string;
+  slug: string;
+  cover: QueryGames_games_cover | null;
+  developers: QueryGames_games_developers[];
+  price: number;
+}
+
+export interface QueryGames {
+  games: QueryGames_games[];
+}
+
+export interface QueryGamesVariables {
+  limit: number;
+}
