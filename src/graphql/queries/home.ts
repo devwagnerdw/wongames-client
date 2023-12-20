817b8bb91f04d2120bc84295377a33069b73b6fc
vagnealves <vagnerdw1@gmail.com>
2023-12-20 18:46:28 -0300

    Create BannerFragment

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,23 +1,12 @@
+import { BannerFragment } from './../fragments/banner'
 import { gql } from '@apollo/client'
 
 // GET_HOME | QUERY_HOME
 export const QUERY_HOME = gql`
   query QueryHome {
     banners {
-      image {
-        url
-      }
-      title
-      subtitle
-      button {
-        label
-        link
-      }
-      ribbon {
-        text
-        color
-        size
-      }
+      ...BannerFragment
     }
   }
+  ${BannerFragment}
 `
