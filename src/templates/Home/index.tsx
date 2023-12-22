fde6a8fab0817406c66e4fa2fc2a3a9024ae73b5
vagnealves <vagnerdw1@gmail.com>
2023-12-22 20:10:28 -0300

    Add option to pass color to Showcase

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -38,7 +38,7 @@ const Home = ({
       </S.SectionBanner>
     </Container>
     <S.SectionNews>
-      <Showcase title="News" games={newGames} />
+      <Showcase title="News" games={newGames} color="black" />
     </S.SectionNews>
     <Showcase
       title="Most Popular"
