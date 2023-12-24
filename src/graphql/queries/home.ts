023d51906af1ac7aa63ee3733717b165bb841365
vagnealves <vagnerdw1@gmail.com>
2023-12-24 17:29:21 -0300

    Update query to include new games

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -10,14 +10,14 @@ export const QUERY_HOME = gql`
       ...BannerFragment
     }
     newGames: games(
-      where: { release_date_lte: "2021-01-27" }
+      where: { release_date_lte: "2024-12-20" }
       sort: "release_date:desc"
       limit: 8
     ) {
       ...GameFragment
     }
     upcomingGames: games(
-      where: { release_date_gt: "2021-01-27" }
+      where: { release_date_gt: "2022-12-23" }
       sort: "release_date:asc"
       limit: 8
     ) {
