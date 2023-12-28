4dfa21fad306f3c590c46200ac4b46d75e6aa256
vagnealves <vagnerdw1@gmail.com>
2023-12-27 22:12:26 -0300

    Change ExploreSidebar to filter always when changing field

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import xor from 'lodash.xor'
 import { Close } from '@styled-icons/material-outlined/Close'
 import { FilterList } from '@styled-icons/material-outlined/FilterList'
@@ -37,6 +37,13 @@ const ExploreSidebar = ({
   const [values, setValues] = useState(initialValues)
   const [isOpen, setIsOpen] = useState(false)
 
+  useEffect(() => {
+    onFilter(values)
+    // this method comes from another template
+    // that we don't have access
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [values])
+
   const handleRadio = (name: string, value: string | boolean) => {
     setValues((s) => ({ ...s, [name]: value }))
   }
@@ -50,8 +57,7 @@ const ExploreSidebar = ({
     setValues((s) => ({ ...s, [name]: xor(currentList, [value]) }))
   }
 
-  const handleFilter = () => {
-    onFilter(values)
+  const handleFilterMenu = () => {
     setIsOpen(false)
   }
   return (
@@ -100,7 +106,7 @@ const ExploreSidebar = ({
         ))}
       </S.Content>
       <S.Footer>
-        <Button fullWidth size="medium" onClick={handleFilter}>
+        <Button fullWidth size="medium" onClick={handleFilterMenu}>
           Filter
         </Button>
       </S.Footer>
--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -1,10 +1,8 @@
 import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
-import { css } from 'styled-components'
 
 import ExploreSidebar from '.'
-import { Overlay } from './styles'
 
 import items from './mock'
 
@@ -65,9 +63,6 @@ describe('<ExploreSidebar />', () => {
       />
     )
 
-    // Simula a interação do usuário clicando no botão de filtro
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
     // Aguarda que a função onFilter seja chamada com os argumentos esperados
     await waitFor(() => {
       expect(onFilter).toBeCalledWith({
@@ -76,27 +71,25 @@ describe('<ExploreSidebar />', () => {
       })
     })
   })
-
-  it('should filter with initial values', async () => {
+  it('should filter with checked values', async () => {
     const onFilter = jest.fn()
-    renderWithTheme(
-      <ExploreSidebar
-        items={items}
-        onFilter={onFilter}
-        initialValues={{ platforms: ['windows'], sort_by: 'low-to-high' }}
-      />
-    )
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
+    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />)
 
-    // Aguarda que a chamada assíncrona seja feita e as operações assíncronas sejam concluídas
-    await waitFor(() => {
-      expect(onFilter).toBeCalledWith({
-        platforms: ['windows'],
-        sort_by: 'low-to-high'
-      })
+    await userEvent.click(screen.getByLabelText(/windows/i))
+    await userEvent.click(screen.getByLabelText(/linux/i))
+    await userEvent.click(screen.getByLabelText(/low to high/i))
+
+    // Aguarda um momento para permitir que as atualizações do estado sejam processadas
+    await new Promise((resolve) => setTimeout(resolve, 0))
+
+    // Verifica se a função onFilter foi chamada com os argumentos esperados
+    expect(onFilter).toHaveBeenCalledTimes(4)
+
+    expect(onFilter).toBeCalledWith({
+      platforms: ['windows', 'linux'],
+      sort_by: 'low-to-high'
     })
   })
-
   it('should altern between radio options', async () => {
     const onFilter = jest.fn()
 
@@ -108,32 +101,11 @@ describe('<ExploreSidebar />', () => {
     // Simula a interação do usuário selecionando a opção "high to low"
     userEvent.click(screen.getByLabelText(/high to low/i))
 
-    // Simula a interação do usuário clicando no botão de filtro
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
     // Aguarda que a função onFilter seja chamada com os argumentos esperados
     await waitFor(() => {
       expect(onFilter).toBeCalledWith({
         sort_by: 'high-to-low'
       })
     })
-  })
-
-  it('should open/close sidebar when filtering on mobile ', () => {
-    const { container } = renderWithTheme(
-      <ExploreSidebar items={items} onFilter={jest.fn} />
-    )
-    const variant = {
-      media: '(max-width:768px)',
-      modifier: String(css`
-        ${Overlay}
-      `)
-    }
-    const Element = container.firstChild
-    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
-    userEvent.click(screen.getByLabelText(/open filters/))
-    expect(Element).toHaveStyleRule('opacity', '1', variant)
-    userEvent.click(screen.getByLabelText(/close filters/))
-    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
   })
 })
