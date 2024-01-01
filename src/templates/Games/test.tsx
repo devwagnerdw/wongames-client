1fde66f3591f99f29da910d8efb482127ef7c6bf
vagnealves <vagnerdw1@gmail.com>
2023-12-31 21:56:48 -0300

    Fix tests on GameTemplate

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -1,36 +1,20 @@
 import { screen } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
 import { renderWithTheme } from 'utils/tests/helpers'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-
-import { MockedProvider } from '@apollo/client/testing'
-
 import { fetchMoreMock, gamesMock, noGamesMock } from './mocks'
 import Games from '.'
 import userEvent from '@testing-library/user-event'
 import apolloCache from 'utils/apolloCache'
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 const push = jest.fn()
-
 useRouter.mockImplementation(() => ({
   push,
   query: '',
   asPath: '',
   route: '/'
 }))
-
-jest.mock('next/router', () => ({
-  useRouter() {
-    return {
-      route: '/',
-      pathname: '',
-      query: '',
-      asPath: ''
-    }
-  }
-}))
-
 jest.mock('templates/Base', () => ({
   __esModule: true,
   default: function Mock({ children }: { children: React.ReactNode }) {
@@ -41,58 +25,55 @@ jest.mock('templates/Base', () => ({
 jest.mock('next/link', () => ({
   __esModule: true,
   default: function Mock({ children }: { children: React.ReactNode }) {
-    return <div data-testid="Mock Base">{children}</div>
+    return <div>{children}</div>
   }
 }))
 
 describe('<Games />', () => {
-  it('should render empty when no games found', async () => {
-    renderWithTheme(
-      <MockedProvider mocks={[noGamesMock]} addTypename={false}>
-        <Games filterItems={filterItemsMock} />
-      </MockedProvider>
-    )
-
-    expect(
-      await screen.findByText(/We didn't find any games with this filter/i)
-    ).toBeInTheDocument()
-  })
-
-  it('should change push router when selecting a filter', async () => {
-    renderWithTheme(
-      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
-        <Games filterItems={filterItemsMock} />
-      </MockedProvider>
-    )
-
-    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
-  })
-
   it('should render sections', async () => {
     renderWithTheme(
       <MockedProvider mocks={[gamesMock]} addTypename={false}>
         <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
-
+    // we wait until we have data to get the elements
+    // get => tem certeza do elemento
+    // query => Não tem o elemento
+    // find => processos assincronos
     expect(await screen.findByText(/Price/i)).toBeInTheDocument()
-
+    expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
     expect(
-      screen.getByRole('button', { name: /Show More/i })
+      await screen.findByRole('button', { name: /show more/i })
     ).toBeInTheDocument()
   })
-
+  it('should render empty when no games found', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[noGamesMock]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+    expect(
+      await screen.findByText(/We didn't find any games with this filter/i)
+    ).toBeInTheDocument()
+  })
   it('should render more games when show more is clicked', async () => {
     renderWithTheme(
       <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
         <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
-
     expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
-
-    userEvent.click(screen.getByRole('button', { name: /Show More/i }))
-
+    userEvent.click(await screen.findByRole('button', { name: /show more/i }))
     expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
+  })
+  it('should change push router when selecting a filter', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
+    userEvent.click(await screen.findByRole('checkbox', { name: /linux/i }))
+    userEvent.click(await screen.findByLabelText(/low to high/i))
   })
 })
