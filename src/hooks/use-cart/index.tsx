2ea5b983f71db04a95419c574cf8df39b542cfcb
vagnealves <vagnerdw1@gmail.com>
2024-01-01 15:35:20 -0300

    Add quantity and total to our useCart

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -3,9 +3,7 @@ import { useContext, createContext, useState, useEffect } from 'react'
 import formatPrice from 'utils/format-price'
 import { getStorageItem } from 'utils/localStorage'
 import { cartMapper } from 'utils/mappers'
-
 const CART_KEY = 'cartItems'
-
 type CartItem = {
   id: string
   img: string
@@ -15,11 +13,16 @@ type CartItem = {
 
 export type CartContextData = {
   items: CartItem[]
+  quantity: number
+  total: string
 }
 
 export const CartContextDefaultValues = {
-  items: []
+  items: [],
+  quantity: 0,
+  total: '$0.00'
 }
+
 export const CartContext = createContext<CartContextData>(
   CartContextDefaultValues
 )
@@ -44,10 +47,16 @@ const CartProvider = ({ children }: CartProviderProps) => {
     }
   })
 
+  const total = data?.games.reduce((acc, game) => {
+    return acc + game.price
+  }, 0)
+
   return (
     <CartContext.Provider
       value={{
-        items: cartMapper(data?.games)
+        items: cartMapper(data?.games),
+        quantity: cartItems.length,
+        total: formatPrice(total || 0)
       }}
     >
       {children}
