b2222155f127e773afd2e9fce97862c1ec1c71fc
vagnealves <vagnerdw1@gmail.com>
2024-01-03 19:22:21 -0300

    Add Context to CartList

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { CartContext, CartContextDefaultValues } from 'hooks/use-cart'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider
+  },
   backgrounds: {
     default: 'won-light',
     values: [
@@ -22,16 +26,18 @@ export const parameters = {
 export const decorators = [
   (Story, context) => (
     <ThemeProvider theme={theme}>
-      <CartContext.Provider
-        value={{
-          ...CartContextDefaultValues,
-          ...(context?.args?.cartContextValue || {}),
-          ...context.args
-        }}
-      >
-        <GlobalStyles removeBg />
-        <Story />
-      </CartContext.Provider>
+      <RouterContext.Provider>
+        <CartContext.Provider
+          value={{
+            ...CartContextDefaultValues,
+            ...(context?.args?.cartContextValue || {}),
+            ...context.args
+          }}
+        >
+          <GlobalStyles removeBg />
+          <Story />
+        </CartContext.Provider>
+      </RouterContext.Provider>
     </ThemeProvider>
   )
 ]
