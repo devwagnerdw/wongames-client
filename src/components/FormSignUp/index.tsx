680ca7c5c8f5af563790b4138b5af03543ac1986
vagnealves <vagnerdw1@gmail.com>
2024-01-07 18:03:38 -0300

    Add useSession to base layout

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
+import { signIn } from 'next-auth/client'
 import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
+
 import { FormWrapper, FormLink, FormLoading } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-
 import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
 import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
@@ -16,7 +17,17 @@ const FormSignUp = () => {
     password: ''
   })
 
-  const [createUser, { loading }] = useMutation(MUTATION_REGISTER)
+  const [createUser, { error, loading }] = useMutation(MUTATION_REGISTER, {
+    onError: (err) => console.error(err),
+    onCompleted: () => {
+      !error &&
+        signIn('credentials', {
+          email: values.email,
+          password: values.password,
+          callbackUrl: '/'
+        })
+    }
+  })
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }))
