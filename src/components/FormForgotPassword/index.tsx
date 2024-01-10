baf9bde3be3d952db8395b04cddf0341545d3f83
vagnealves <vagnerdw1@gmail.com>
2024-01-10 18:41:29 -0300

    Reset Password flow functionality

--- src/components/FormForgotPassword/index.tsx
+++ src/components/FormForgotPassword/index.tsx
@@ -1,28 +1,17 @@
 import { useState } from 'react'
-
-import {
-  CheckCircleOutline,
-  Email,
-  ErrorOutline
-} from '@styled-icons/material-outlined'
-
-import {
-  FormWrapper,
-  FormLoading,
-  FormError,
-  FormSuccess
-} from 'components/Form'
+import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import { Lock, ErrorOutline } from '@styled-icons/material-outlined'
+import { FormWrapper, FormLoading, FormError } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-
-import { FieldErrors, forgotValidate } from 'utils/validations'
-
-const FormForgotPassword = () => {
-  const [success, setSuccess] = useState(false)
+import { FieldErrors, resetValidate } from 'utils/validations'
+const FormResetPassword = () => {
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
-  const [values, setValues] = useState({ email: '' })
+  const [values, setValues] = useState({ password: '', confirm_password: '' })
   const [loading, setLoading] = useState(false)
+  const { query } = useRouter()
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }))
@@ -31,9 +20,7 @@ const FormForgotPassword = () => {
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
     setLoading(true)
-
-    const errors = forgotValidate(values)
-
+    const errors = resetValidate(values)
     if (Object.keys(errors).length) {
       setFieldError(errors)
       setLoading(false)
@@ -42,60 +29,64 @@ const FormForgotPassword = () => {
 
     setFieldError({})
 
-    // enviar um post para /forgot-password pedindo um email
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
       {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(values)
+        body: JSON.stringify({
+          password: values.password,
+          passwordConfirmation: values.confirm_password,
+          code: query.code
+        })
       }
     )
 
     const data = await response.json()
-    setLoading(false)
 
     if (data.error) {
       setFormError(data.message[0].messages[0].message)
+      setLoading(false)
     } else {
-      setSuccess(true)
+      signIn('credentials', {
+        email: data.user.email,
+        password: values.password,
+        callbackUrl: '/'
+      })
     }
   }
 
   return (
     <FormWrapper>
-      {success ? (
-        <FormSuccess>
-          <CheckCircleOutline />
-          You just received an email!
-        </FormSuccess>
-      ) : (
-        <>
-          {!!formError && (
-            <FormError>
-              <ErrorOutline /> {formError}
-            </FormError>
-          )}
-          <form onSubmit={handleSubmit}>
-            <TextField
-              name="email"
-              placeholder="Email"
-              type="text"
-              error={fieldError?.email}
-              onInputChange={(v) => handleInput('email', v)}
-              icon={<Email />}
-            />
-
-            <Button type="submit" size="large" fullWidth disabled={loading}>
-              {loading ? <FormLoading /> : <span>Send email</span>}
-            </Button>
-          </form>
-        </>
+      {!!formError && (
+        <FormError>
+          <ErrorOutline /> {formError}
+        </FormError>
       )}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          error={fieldError?.password}
+          onInputChange={(v) => handleInput('password', v)}
+          icon={<Lock />}
+        />
+        <TextField
+          name="confirm_password"
+          placeholder="Confirm password"
+          type="password"
+          error={fieldError?.confirm_password}
+          onInputChange={(v) => handleInput('confirm_password', v)}
+          icon={<Lock />}
+        />
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Reset Password</span>}
+        </Button>
+      </form>
     </FormWrapper>
   )
 }
-
-export default FormForgotPassword
+export default FormResetPassword
