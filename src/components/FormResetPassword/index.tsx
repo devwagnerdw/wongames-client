14232fb0a01878f037c3dad266389187e265bd16
vagnealves <vagnerdw1@gmail.com>
2024-01-10 19:59:05 -0300

    Change FormProfile to show ResetPassword Link

--- src/components/FormResetPassword/index.tsx
+++ src/components/FormResetPassword/index.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
 import { signIn } from 'next-auth/client'
 import { useRouter } from 'next/router'
-
 import { Lock, ErrorOutline } from '@styled-icons/material-outlined'
-
 import { FormWrapper, FormLoading, FormError } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-
 import { FieldErrors, resetValidate } from 'utils/validations'
-
 const FormResetPassword = () => {
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState({ password: '', confirm_password: '' })
   const [loading, setLoading] = useState(false)
-  const routes = useRouter()
-  const { push, query } = routes
+  const { query } = useRouter()
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }))
   }
-
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
     setLoading(true)
-
     const errors = resetValidate(values)
-
     if (Object.keys(errors).length) {
       setFieldError(errors)
       setLoading(false)
@@ -36,21 +28,33 @@ const FormResetPassword = () => {
 
     setFieldError({})
 
-    // sign in
-    const result = await signIn('credentials', {
-      ...values,
-      redirect: false,
-      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
-    })
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          password: values.password,
+          passwordConfirmation: values.confirm_password,
+          code: query.code
+        })
+      }
+    )
 
-    if (result?.url) {
-      return push(result?.url)
+    const data = await response.json()
+
+    if (data.error) {
+      setFormError(data.message[0].messages[0].message)
+      setLoading(false)
+    } else {
+      signIn('credentials', {
+        email: data.user.email,
+        password: values.password,
+        callbackUrl: '/'
+      })
     }
-
-    setLoading(false)
-
-    // jogar o erro
-    setFormError('username or password is invalid')
   }
 
   return (
@@ -77,7 +81,6 @@ const FormResetPassword = () => {
           onInputChange={(v) => handleInput('confirm_password', v)}
           icon={<Lock />}
         />
-
         <Button type="submit" size="large" fullWidth disabled={loading}>
           {loading ? <FormLoading /> : <span>Reset Password</span>}
         </Button>
@@ -85,5 +88,4 @@ const FormResetPassword = () => {
     </FormWrapper>
   )
 }
-
 export default FormResetPassword
