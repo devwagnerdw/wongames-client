1711deea488292110ba9c557eec707d29a355c19
vagnealves <vagnerdw1@gmail.com>
2024-01-09 21:28:39 -0300

    Add ProfileQuery and values to /profile/me page

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -1,25 +1,33 @@
 import Button from 'components/Button'
 import Heading from 'components/Heading'
 import TextField from 'components/TextField'
+
 import * as S from './styles'
 
-const FormProfile = () => (
+export type FormProfileProps = {
+  username?: string
+  email?: string
+}
+
+const FormProfile = ({ email, username }: FormProfileProps) => (
   <>
     <Heading lineBottom color="black" size="small">
       My profile
     </Heading>
+
     <S.Form>
       <TextField
-        name="name"
-        placeholder="Name"
-        label="Name"
-        initialValue="John Doe"
+        name="username"
+        placeholder="Username"
+        label="Username"
+        initialValue={username}
       />
+
       <TextField
         name="email"
         type="email"
         placeholder="E-mail"
-        initialValue="johndoe@gmail.com"
+        initialValue={email}
         label="E-mail"
         disabled
       />
@@ -35,10 +43,8 @@ const FormProfile = () => (
         placeholder="New password"
         label="New password"
       />
-
       <Button size="large">Save</Button>
     </S.Form>
   </>
 )
-
 export default FormProfile
