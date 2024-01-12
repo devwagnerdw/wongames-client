307dc7023c1e0c905cb641f25d6338a69cb70990
vagnealves <vagnerdw1@gmail.com>
2024-01-11 21:33:56 -0300

    Add Wishlist Mutations and its types

--- src/graphql/generated/globalTypes.ts
+++ src/graphql/generated/globalTypes.ts
@@ -2,26 +2,21 @@
 /* eslint-disable */
 // @generated
 // This file was automatically generated and should not be edited.
-
 //==============================================================
 // START Enums and Input Objects
 //==============================================================
-
 export enum ENUM_COMPONENTPAGEHIGHLIGHT_ALIGNMENT {
   left = "left",
   right = "right",
 }
-
 export enum ENUM_COMPONENTPAGERIBBON_COLOR {
   primary = "primary",
   secondary = "secondary",
 }
-
 export enum ENUM_COMPONENTPAGERIBBON_SIZE {
   normal = "normal",
   small = "small",
 }
-
 export enum ENUM_GAME_RATING {
   BR0 = "BR0",
   BR10 = "BR10",
@@ -31,12 +26,39 @@ export enum ENUM_GAME_RATING {
   BR18 = "BR18",
 }
 
+export interface InputID {
+  id: string;
+}
+
 export interface UsersPermissionsRegisterInput {
   username: string;
   email: string;
   password: string;
 }
 
+export interface WishlistInput {
+  user?: string | null;
+  games?: (string | null)[] | null;
+  created_by?: string | null;
+  updated_by?: string | null;
+}
+
+export interface createWishlistInput {
+  data?: WishlistInput | null;
+}
+
+export interface editWishlistInput {
+  user?: string | null;
+  games?: (string | null)[] | null;
+  created_by?: string | null;
+  updated_by?: string | null;
+}
+
+export interface updateWishlistInput {
+  where?: InputID | null;
+  data?: editWishlistInput | null;
+}
+
 //==============================================================
 // END Enums and Input Objects
-//===================
+//==============================================================
