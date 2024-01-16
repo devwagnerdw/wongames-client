1838682b4527eb05d439df4e51d9bbd4b893acf1
vagnealves <vagnerdw1@gmail.com>
2024-01-15 22:27:47 -0300

    Add Success page

--- src/pages/profile/cards.tsx
+++ src/pages/profile/cards.tsx
@@ -2,8 +2,6 @@ import CardsList, { CardsListProps } from 'components/CardsList'
 import Profile from 'templates/Profile'
 
 import mockCards from 'components/PaymentOptions/mock'
-
-
 import { GetServerSidePropsContext } from 'next'
 import protectedRoutes from 'utils/protected-routes'
 
