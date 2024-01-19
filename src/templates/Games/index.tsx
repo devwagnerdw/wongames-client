09d8fbdb709cce0fa419dabb2cec51ddcd90cee7
vagnealves <vagnerdw1@gmail.com>
2024-01-19 20:30:48 -0300

    Add getImageUrl

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -7,8 +7,11 @@ import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import GameCard from 'components/GameCard'
 import { Grid } from 'components/Grid'
+
 import * as S from './styles'
 import Empty from 'components/Empty'
+import { getImageUrl } from 'utils/getImageUrl'
+
 export type GamesTemplateProps = {
   filterItems: ItemProps[]
 }
@@ -61,7 +64,7 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
                     title={game.name}
                     slug={game.slug}
                     developer={game.developers[0].name}
-                    img={`http://localhost:1337${game.cover?.url}`}
+                    img={`${getImageUrl(game.cover!.url)}`}
                     price={game.price}
                   />
                 ))}
