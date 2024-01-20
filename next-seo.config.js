c4f03f670949079a28da9612db2d60f591460cde
vagnealves <vagnerdw1@gmail.com>
2024-01-20 13:16:16 -0300

    Add NextSEO default

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.wongames.vagnealves.com.br',
+    site_name: 'Won Games',
+    title: 'Won Games'
+  },
+  twitter: {
+    handle: '@VagnerAzx',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
