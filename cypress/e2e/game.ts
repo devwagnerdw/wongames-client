6a9a9b0618fcef59296e56e47c9995d647207a5c
vagnealves <vagnerdw1@gmail.com>
2024-01-25 18:22:00 -0300

    Get children and textContent inside Game Page

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -4,11 +4,18 @@ describe('Game Page', () => {
   it('should render game page sections', () => {
     cy.visit('/game/cyberpunk-2077');
 
-    cy.getByDataCy('game-info').should('exist').within(() => {
+    cy.getByDataCy('game-info', { timeout: 70000 }).should('exist').within(() => {
       cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist');
     });
 
     // gallery
     cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0);
+
+      // content
+      cy.getByDataCy('content').within(() => {
+        cy.findByRole('heading', { name: /description/i }).should('exist')
+      })
+  
+      cy.getByDataCy('content').children().should('have.length.at.least', 2)
   });
 });
