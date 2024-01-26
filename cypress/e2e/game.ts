e5debfe0a6dada03b1d222e802901b2d853a279f
vagnealves <vagnerdw1@gmail.com>
2024-01-26 20:00:38 -0300

    Add game to cart via Game Page

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,24 +1,26 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Game Page', () => {
+  before(() => {
+    cy.visit('/game/cyberpunk-2077')
+  })
+  
   it('should render game page sections', () => {
-    cy.visit('/game/cyberpunk-2077');
-
-    cy.getByDataCy('game-info', { timeout: 70000 }).should('exist').within(() => {
-      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist');
-    });
+    cy.getByDataCy('game-info', { timeout: 70000 }).within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+      cy.findByText(/^Cyberpunk 2077 is an open-world/i).should('exist')
+      cy.findByText('$59.99').should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
 
     // gallery
-    cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0);
-
-      // content
-      cy.getByDataCy('content').within(() => {
-        cy.findByRole('heading', { name: /description/i }).should('exist')
-      })
-  
-      cy.getByDataCy('content').children().should('have.length.at.least', 2)
-
-      // Details
+    cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0)
+    // content
+    cy.getByDataCy('content').within(() => {
+      cy.findByRole('heading', { name: /description/i }).should('exist')
+    })
+    cy.getByDataCy('content').children().should('have.length.at.least', 2)
+    // Details
     cy.getByDataCy('game-details').within(() => {
       cy.findByRole('heading', { name: /game details/i }).should('exist')
       cy.findByRole('heading', { name: /developer/i }).should('exist')
@@ -37,5 +39,21 @@ describe('Game Page', () => {
 
     cy.shouldRenderShowcase({ name: "Upcoming Games", hightlight: true})
     cy.shouldRenderShowcase({ name: "You may like these games", hightlight: false})
+  });
+
+  it('should add/remove game in cart', () => {
+    cy.getByDataCy('game-info',{ timeout: 70000 }).within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).click()
+      cy.findByRole('button', { name: /remove from cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i)
+      .first()
+      .should('have.length', 1)
+      .click()
+
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+    })
   });
 });
