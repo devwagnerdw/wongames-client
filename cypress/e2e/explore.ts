5fcbba8754d47efa99fc62f6bb6b1f50c17c9af4
vagnealves <vagnerdw1@gmail.com>
2024-01-26 20:18:03 -0300

    Add test to show more

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -1,16 +1,27 @@
 /// <reference path="../support/index.d.ts" />
 import { priceFields, genreFields, platformFields, sortFields} from '../../src/utils/filter/fields'
+
 describe('Explore Page', () => {
-  it('should render filters columns', () => {
+  before(() => {
     cy.visit('/games')
+  })
+
+  it('should render filters columns', () => {
+
     cy.findByRole('heading', { name: /sort by price/i }).should('exist')
     cy.findByRole('heading', { name: /^price/i }).should('exist')
     cy.findByRole('heading', { name: /platforms/i }).should('exist')
     cy.findByRole('heading', { name: /genres/i }).should('exist')
-
+    
     cy.getFields(sortFields)
     cy.getFields(priceFields)
     cy.getFields(platformFields)
     cy.getFields(genreFields)
+  });
+
+  it('should show 15 games and show more games when show more is clicked', () => {
+    cy.getByDataCy('game-card').should('have.length', 15)
+    cy.findByRole('button', { name: /show more/i }).click()
+    cy.getByDataCy('game-card').should('have.length', 30)
   });
 });
