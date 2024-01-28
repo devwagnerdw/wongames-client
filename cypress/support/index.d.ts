43d2cb9d48c5c53319a6e34c19c0db103c326e69
vagnealves <vagnerdw1@gmail.com>
2024-01-28 14:34:23 -0300

    Create signUp custom command

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -11,6 +11,12 @@ type FieldsAttributes = {
   name: string | number
 }
 
+type User = {
+  username: string
+  email: string
+  password: string
+}
+
 declare namespace Cypress {
   interface Chainable {
     /**
@@ -18,6 +24,14 @@ declare namespace Cypress {
      * @example cy.google()
      */
     google(): Chainable<Window>
+
+    /**
+     * Custom command to sign up
+     * @example cy.signUp(user)
+     */
+    signUp(user: User): Chainable<Element>
+
+
     /**
      * Custom command to get element by data-cy
      * @example cy.getByDataCy('selector')
@@ -41,8 +55,6 @@ declare namespace Cypress {
      * @example cy.shouldRenderShowcase()
      */
     shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
-
-
     /**
      * Custom command to check if value is less than
      * @example cy.shouldBeLessThan(100)
