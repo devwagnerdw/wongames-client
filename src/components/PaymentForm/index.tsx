925d60fdf8447dbddf682f40a9860a509ec26b89
vagnealves <vagnerdw1@gmail.com>
2024-01-28 16:17:24 -0300

    Fixed type errors with 'session' from 'next-auth/client

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -4,40 +4,49 @@ import { useRouter } from 'next/router'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
 import { PaymentIntent, StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
+
 import { useCart } from 'hooks/use-cart'
 import Button from 'components/Button'
 import Heading from 'components/Heading'
+
 import * as S from './styles'
 import { createPayment, createPaymentIntent } from 'utils/stripe/methods'
-import { Session } from 'next-auth/client'
+
 import { FormLoading } from 'components/Form'
+import { Session } from 'next-auth'
+
 type PaymentFormProps = {
   session: Session
 }
+
 const PaymentForm = ({ session }: PaymentFormProps) => {
   const { items } = useCart()
   const { push } = useRouter()
   const stripe = useStripe()
   const elements = useElements()
+
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
   const [disabled, setDisabled] = useState(true)
   const [clientSecret, setClientSecret] = useState('')
   const [freeGames, setFreeGames] = useState(false)
+
   useEffect(() => {
     async function setPaymentMode() {
       if (items.length) {
         // bater na API /orders/create-payment-intent
         const data = await createPaymentIntent({
           items,
-          token: session.jwt
+          token: session.jwt as string
         })
+
         // se eu receber freeGames: true => setFreeGames
         // faço o fluxo de jogo gratuito
         if (data.freeGames) {
           setFreeGames(true)
           return
         }
+
         // se eu receber um erro
         // setError
         if (data.error) {
@@ -50,50 +59,62 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
         }
       }
     }
+
     setPaymentMode()
   }, [items, session])
+
   const handleChange = async (event: StripeCardElementChangeEvent) => {
     setDisabled(event.empty)
     setError(event.error ? event.error.message : '')
   }
+
   const saveOrder = async (paymentIntent?: PaymentIntent) => {
     const data = await createPayment({
       items,
       paymentIntent,
-      token: session.jwt
+      token: session.jwt as string
     })
+
     return data
   }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
     setLoading(true)
+
     // se for freeGames
     if (freeGames) {
       // salva no banco
       // bater na API /orders
       saveOrder()
+
       // redireciona para success
       push('/success')
       return
     }
+
     const payload = await stripe!.confirmCardPayment(clientSecret, {
       payment_method: {
         card: elements!.getElement(CardElement)!
       }
     })
+
     if (payload.error) {
       setError(`Payment failed ${payload.error.message}`)
       setLoading(false)
     } else {
       setError(null)
       setLoading(false)
+
       // salvar a compra no banco do Strapi
       // bater na API /orders
       saveOrder(payload.paymentIntent)
+
       // redirectionar para a página de Sucesso
       push('/success')
     }
   }
+
   return (
     <S.Wrapper>
       <form onSubmit={handleSubmit}>
@@ -101,6 +122,7 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
           <Heading color="black" size="small" lineBottom>
             Payment
           </Heading>
+
           {freeGames ? (
             <S.FreeGames>Only free games, click buy and enjoy!</S.FreeGames>
           ) : (
@@ -116,6 +138,7 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
               onChange={handleChange}
             />
           )}
+
           {error && (
             <S.Error>
               <ErrorOutline size={20} />
@@ -141,4 +164,5 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     </S.Wrapper>
   )
 }
+
 export default PaymentForm
