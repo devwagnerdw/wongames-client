0db884f837bc518dd7b9448e231b0a11c1dbd85e
vagnealves <vagnerdw1@gmail.com>
2024-02-02 19:28:10 -0300

    Add paid games flow

--- cypress/e2e/checkout.ts
+++ cypress/e2e/checkout.ts
@@ -7,8 +7,52 @@ describe('Checkout', () => {
     before(() => {
       user = createUser()
     })
-
     it('should buy free games', () => {
+      // criar um usuário
+      cy.visit('/sign-up')
+      cy.signUp(user)
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+      // ir para explore page
+      cy.findByRole('link', { name: /explore/i }).click()
+      cy.url().should('eq', `${Cypress.config().baseUrl}/games`)
+      // filtrar por jogos free
+      cy.findByText(/free/i).click()
+      cy.url().should('contain', 'price_lte=0')
+      // adicionar o jogo no carrinho
+      cy.addToCartByIndex(0)
+      // verificar se o carrinho tem 1 jogo e abrir dropdown
+      cy.findAllByLabelText(/cart items/i)
+        .first()
+        .should('have.text', 1)
+        .click()
+      // clicar para fazer a compra
+      cy.getByDataCy('cart-list').within(() => {
+        cy.findByText(/buy it now/i).click()
+      })
+      // encontrar um texto de só jogos free
+      cy.findByText(/Only free games, click buy and enjoy!/i).should('exist')
+      // clicar para comprar
+      cy.findByRole('button', { name: /buy now/i }).click()
+      // redirecionar para a página de success
+      cy.url().should('eq', `${Cypress.config().baseUrl}/success`)
+      // mostrar texto de sucesso
+      cy.findByRole('heading', { name: /Your purchase was successful!/i }).should('exist')
+    });
+    it('should show games in order page', () => {
+      cy.visit('/profile/orders')
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/orders`)
+      cy.signIn(user.email, user.password)
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/orders`)
+      cy.getByDataCy('game-item').should('have.length', 1)
+    });
+  });
+
+  describe('Paid Games', () => {
+    before(() => {
+      user = createUser()
+    })
+
+    it('should buy paid games', () => {
       // criar um usuário
       cy.visit('/sign-up')
       cy.signUp(user)
@@ -19,8 +63,8 @@ describe('Checkout', () => {
       cy.url().should('eq', `${Cypress.config().baseUrl}/games`)
 
       // filtrar por jogos free
-      cy.findByText(/free/i).click()
-      cy.url().should('contain', 'price_lte=0')
+      cy.findByText(/highest to lowest/i).click()
+      cy.location('href').should('contain', 'sort=price%3Adesc')
 
       // adicionar o jogo no carrinho
       cy.addToCartByIndex(0)
@@ -36,14 +80,19 @@ describe('Checkout', () => {
         cy.findByText(/buy it now/i).click()
       })
 
-      // encontrar um texto de só jogos free
-      cy.findByText(/Only free games, click buy and enjoy!/i).should('exist')
+      // o botão de comprar deve estar desabilitado
+      cy.findByRole('button', { name: /buy now/i }).should('have.attr', 'disabled')
+
+      // preencher com o cartão de credito
+      cy.fillElementsInput('cardNumber', '4242424242424242')
+      cy.fillElementsInput('cardExpiry', '1040')
+      cy.fillElementsInput('cardCvc', '103')
 
       // clicar para comprar
       cy.findByRole('button', { name: /buy now/i }).click()
 
       // redirecionar para a página de success
-      cy.url().should('eq', `${Cypress.config().baseUrl}/success`)
+      cy.url({ timeout: 30000 }).should('eq', `${Cypress.config().baseUrl}/success`)
 
       // mostrar texto de sucesso
       cy.findByRole('heading', { name: /Your purchase was successful!/i }).should('exist')
@@ -58,9 +107,5 @@ describe('Checkout', () => {
 
       cy.getByDataCy('game-item').should('have.length', 1)
     });
-  });
-
-  describe('Paid Games', () => {
-
   });
 });
